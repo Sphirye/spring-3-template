c56bbb86da2f81cb715c3b21216667565a8014b4
sphirye <ronielcontreras70@gmail.com>
2023-08-08 14:05:31 -0500

    Refactor private property names and correctly indent

--- src/main/kotlin/com/sphirye/springtemplate/security/CustomUserDetailsService.kt
+++ src/main/kotlin/com/sphirye/springtemplate/security/CustomUserDetailsService.kt
@@ -6,23 +6,22 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component("userDetailsService")
 class CustomUserDetailsService : UserDetailsService {
 
-    @Autowired private lateinit var userService: UserService
+    @Autowired
+    private lateinit var _userService: UserService
 
+    @Transactional
     override fun loadUserByUsername(email: String): UserDetails {
-        val user = userService.findByEmail(email)
-
+        val user = _userService.findByEmail(email)
         return _createUserDetails(user)
+
     }
 
     private fun _createUserDetails(user: com.sphirye.springtemplate.model.User): User {
-        return User(
-            user.email,
-            user.password,
-            listOf(),
-        )
+        return User(user.email, user.password, listOf())
     }
 }
