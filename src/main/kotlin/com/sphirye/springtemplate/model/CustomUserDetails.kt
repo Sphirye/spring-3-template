fda5ed9d4e0fddbad5e2df18bcf4f7f17763f6bc
sphirye <ronielcontreras70@gmail.com>
2023-08-10 12:46:42 -0500

    Create CustomUserDetails model

---
+++ src/main/kotlin/com/sphirye/springtemplate/model/CustomUserDetails.kt
@@ -0,0 +1,46 @@
+package com.sphirye.springtemplate.model
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+data class CustomUser (
+    private val id: Long?,
+    private val username: String?,
+    private val email: String?,
+    private val password: String?,
+    private val authorities: Collection<SimpleGrantedAuthority>?
+) :
+    UserDetails {
+    private val accountExpiredYn = false
+    private val accountLockedYn = false
+    private val credentialsExpiredYn = false
+    private val enabledYn = true
+    override fun getUsername(): String? {
+        return username
+    }
+
+    override fun getPassword(): String? {
+        return password
+    }
+
+    override fun getAuthorities(): Collection<GrantedAuthority?>? {
+        return authorities
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return !accountExpiredYn
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return !accountLockedYn
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return !credentialsExpiredYn
+    }
+
+    override fun isEnabled(): Boolean {
+        return enabledYn
+    }
+}
