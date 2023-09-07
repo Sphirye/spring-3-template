5a234fc886e3d8ed29d59ed7d9c239b623f9544c
sphirye <ronielcontreras70@gmail.com>
2023-09-07 12:15:33 -0500

    Replace mysql connector to postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,7 @@ dependencies {
 
 	//DATABASE
 	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	runtimeOnly("org.postgresql:postgresql")
 
 	//JWT
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
