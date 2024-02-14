39cf3984b7be371291a8ff7f3d0ba42cb8306500
sphirye <ronielcontreras70@gmail.com>
2024-02-14 15:03:26 -0500

    Implement base service in AuthorityService.kt

--- src/main/kotlin/com/sphirye/springtemplate/model/Authority.kt
+++ src/main/kotlin/com/sphirye/springtemplate/model/Authority.kt
@@ -1,6 +1,7 @@
 package com.sphirye.springtemplate.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.sphirye.shared.utils.Identifiable
 import jakarta.persistence.*
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
@@ -10,7 +11,7 @@ class Authority (
     @Id
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    var role: Role,
+    override var id: Role?,
 
     var description: String? = null,
 
@@ -21,14 +22,14 @@ class Authority (
     @Column(nullable = false)
     var enabled: Boolean? = true,
 
-    ) {
+    ): Identifiable<Authority.Role> {
     enum class Role {
         SUPER_ADMIN, ADMIN, MOD
     }
 
     companion object {
         fun getSimpleGrantedAuthoritiesFrom(authorities: MutableSet<Authority>): List<SimpleGrantedAuthority> {
-            return authorities.map { it.role.toString() }.map { SimpleGrantedAuthority(it) }
+            return authorities.map { it.id.toString() }.map { SimpleGrantedAuthority(it) }
         }
     }
 }
--- src/main/kotlin/com/sphirye/springtemplate/service/AuthorityService.kt
+++ src/main/kotlin/com/sphirye/springtemplate/service/AuthorityService.kt
@@ -1,22 +1,15 @@
 package com.sphirye.springtemplate.service
 
+import com.sphirye.shared.utils.BaseService
 import com.sphirye.springtemplate.model.Authority
 import com.sphirye.springtemplate.repository.AuthorityRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class AuthorityService {
-
-    @Autowired
-    private lateinit var _authorityRepository: AuthorityRepository
-
-    @Autowired
-    private lateinit var _userService: UserService
-
-    fun create(authority: Authority): Authority {
-        return _authorityRepository.save(authority)
-    }
+class AuthorityService(
+    private var _authorityRepository: AuthorityRepository,
+): BaseService<Authority, Authority.Role>(_authorityRepository) {
 
     fun findByRole(role: Authority.Role): Authority {
         return _authorityRepository.getReferenceById(role)
