7e2c2a23adaa93afb6982bd6cb273168681678dd
sphirye <ronielcontreras70@gmail.com>
2024-02-15 06:37:16 -0500

    Add Constants.kt

---
+++ src/main/kotlin/com/sphirye/springtemplate/service/tools/Constants.kt
@@ -0,0 +1,12 @@
+package com.sphirye.springtemplate.service.tools
+
+object Constants {
+
+    const val X_TOTAL_COUNT_HEADER = "X-Total-Count"
+    const val X_TOTAL_PAGES_HEADER = "X-Pager-Total-Pages"
+    const val X_PAGE_SIZE_HEADER = "X-Pager-Page-Size"
+
+    const val API_REQUEST_DEFAULT_PAGE = 0
+    const val API_REQUEST_DEFAULT_PAGE_SIZE = 20
+
+}
